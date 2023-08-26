d79edd2ad467255a0d7a528f33d9c9d471e2965c
iceprey <abuwswork@gmail.com>
2023-08-26 10:33:27 -0700

    init

--- bot.js
+++ bot.js
@@ -1,15 +1,47 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 import "dotenv/config";
+import path from "node:path";
+import fs from "fs";
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
-client.on("interactionCreate", async (interaction) => {
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log`[WARN] The command at ${filePath} is missing a required "data" or "execute property`;
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
-  if (interaction.commandName === "ping") {
-    await interaction.reply("Pong");
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName}`);
+    return;
+  }
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command",
+        ephemeral: true,
+      });
+    }
   }
 });
 
---
+++ commands/ping.js
@@ -0,0 +1,8 @@
+import { SlashCommandBuilder } from "discord.js";
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("ping").setDescription("test"),
+  async execute(interaction) {
+    await interaction.reply("pong");
+  },
+};
