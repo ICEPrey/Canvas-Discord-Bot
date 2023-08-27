95edc80fcf048d89f97943b7eecda4bf3d981e0f
iceprey <abuwswork@gmail.com>
2023-08-27 08:34:29 -0700

    command handling

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-import { SlashCommandBuilder } from "discord.js";
-
-module.exports = {
-  data: new SlashCommandBuilder().setName("ping").setDescription("test"),
-  async execute(interaction) {
-    await interaction.reply("pong");
-  },
-};
